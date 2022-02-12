3212a7ed7ceba32304b2c29185c0f32f96197dc6
Dmitry Trokhlib <fgh98zip@gmail.com>
2022-02-12 13:30:32 +0200

    commit test-nats project

---
+++ src/publisher.ts
@@ -0,0 +1,33 @@
+import nats, { Message } from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
+const stan = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', async () => {
+  console.log('Publisher connected to NATS');
+
+  const publisher = new TicketCreatedPublisher(stan);
+
+  try {
+    await publisher.publish({
+      id: 'dasadas',
+      title: 'adasddsa',
+      price: 23,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  // // Publisher
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event Published');
+  // });
+});
